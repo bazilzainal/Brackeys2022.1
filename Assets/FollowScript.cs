7ca9eef3e2e567582bc43c700b139d2bea8ba2e4
Bazil Zainal <bazilzainal@gmail.com>
2022-02-22 09:15:31 +0800

    Add slider to modify max distance of entities to player

--- Assets/FollowScript.cs
+++ Assets/FollowScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class FollowScript : MonoBehaviour
 {
+
+    [SerializeField] Slider mSlider;
     private float distToPlayer;
     [SerializeField] GameObject player;
     [SerializeField] float followSpeed = 10f;
@@ -13,12 +17,15 @@ public class FollowScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        mSlider.value = magDeltaLimit;
+        mSlider.maxValue = 10f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        magDeltaLimit = mSlider.value;
+
         distToPlayer = Vector3.Distance(player.transform.position, transform.position);
 
         // float magDelta = (transform.position - player.transform.position).magnitude;
