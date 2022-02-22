042997e3b070448096fed13ba28c12d1524d0765
marangsana <100122039+marangsana@users.noreply.github.com>
2022-02-22 16:21:28 +0800

    Jittery Queen
    Randomized some movement

--- Assets/FollowScript.cs
+++ Assets/FollowScript.cs
@@ -6,9 +6,16 @@ public class FollowScript : MonoBehaviour
 {
     private float distToPlayer;
     [SerializeField] GameObject player;
-    [SerializeField] float followSpeed = 10f;
+    [SerializeField] float followSpeedMin = 10f;
+    [SerializeField] float followSpeedMax = 20f;
+    [SerializeField] float distanceAwayMin = 1f;
+    [SerializeField] float distanceAwayMax = 3f;
+    [SerializeField] float distanceError = 3f;
+    [SerializeField] int countLimit = 5;
+    int count = 0;
+    float magDeltaLimit = 0;
+    float followSpeed = 0;
 
-    [SerializeField] float magDeltaLimit = 1f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,13 +26,19 @@ public class FollowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        distToPlayer = Vector3.Distance(player.transform.position, transform.position);
+        if (count>countLimit){
+            magDeltaLimit = Random.Range(distanceAwayMin,distanceAwayMax);
+            followSpeed = Random.Range(followSpeedMin,followSpeedMax);
+            distToPlayer = Vector3.Distance(player.transform.position, transform.position) + Random.Range(-distanceError,distanceError);
+            count = 0;
+        }
 
         // float magDelta = (transform.position - player.transform.position).magnitude;
         if (distToPlayer > magDeltaLimit)
         {
             transform.position = Vector3.MoveTowards(transform.position, player.transform.position, followSpeed * Time.deltaTime);
-
         }
+
+        count++;
     }
 }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -20,12 +20,12 @@ public class PlayerController : MonoBehaviour
 
         if (horizontalInput != 0)
         {
-            transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0));
+            transform.Translate(new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0, 0) + new Vector3(Random.Range(-0.05f,0.04f),0,0));
         }
 
         if (verticalInput != 0)
         {
-            transform.Translate(new Vector3(0, verticalInput * moveSpeed * Time.deltaTime, 0));
+            transform.Translate(new Vector3(0, verticalInput * moveSpeed * Time.deltaTime, 0) + new Vector3(0,Random.Range(-0.05f,0.04f),0));
         }
     }
 }
