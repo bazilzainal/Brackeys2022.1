a554441d3a9a7b1005b5017847ff4fd5a651e733
Bazil Zainal <bazilzainal@gmail.com>
2022-02-25 16:48:28 +0800

    Add GameManager, Death triggers, and Parallax scripts

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -28,7 +28,6 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
         xRaw = Input.GetAxisRaw("Horizontal");
         yRaw = Input.GetAxisRaw("Vertical");
         
