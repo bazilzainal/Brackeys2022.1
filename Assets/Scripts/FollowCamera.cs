3f4ec4ee8a7d77a83e9595b27ccddfdc70edabda
Bazil Zainal <bazilzainal@gmail.com>
2022-02-25 15:13:58 +0800

    Fix player sticking to wall bug

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject cam;
+    [SerializeField] float offset = 2f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(cam.transform.position.x + offset, cam.transform.position.y, cam.transform.position.z);
+    }
+}
