931ed6dd1e343aad26ad7b8d7f92e029b62167b6
Bazil Z <bazilzainal@gmail.com>
2022-02-26 22:02:44 +0800

    Add parallax BG assets

--- Assets/Scripts/PushScript.cs
+++ Assets/Scripts/PushScript.cs
@@ -7,6 +7,9 @@ public class PushScript : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float currX;
+    [SerializeField] float currY;
+
+    [SerializeField] float worldYLimit = -10f;
 
     private void Start() {
         currX = transform.position.x;
@@ -16,17 +19,28 @@ public class PushScript : MonoBehaviour
     void Update()
     {
         float playerX = player.transform.position.x;
+
         currX += moveSpeed * Time.deltaTime;
+
+        currY = player.transform.position.y;
+
+        if (currY < worldYLimit)
+        {
+            currY = worldYLimit;
+        }
 
 
         if (playerX <= currX)
         {
-            transform.position = new Vector3(currX, player.transform.position.y, player.transform.position.z);
+
+            transform.position = new Vector3(currX, currY, player.transform.position.z);
 
         } else {
-            transform.position = new Vector3(playerX, player.transform.position.y, player.transform.position.z);
+            transform.position = new Vector3(playerX, currY, player.transform.position.z);
             currX = playerX;
         }
-        
+
+
+
     }
 }
